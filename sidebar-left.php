83b061845b98ed9af1d1ae8512022d7772ff0333
Chirag Gude <chiraggude1@gmail.com>
2014-03-15 22:16:58 +0000

    Added warning if no widgets found in sidebars

--- sidebar-left.php
+++ sidebar-left.php
@@ -1,5 +1,10 @@
 <br/><br/>
 <div class="well well-sm">
-<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('left-sidebar') ) : ?>
-<?php endif; ?>
+<?php if ( !function_exists('dynamic_sidebar')  || !dynamic_sidebar( 'left-sidebar' ) ) :  ?>  
+                <div class="alert alert-warning">
+					<strong>NO WIDGETS FOUND!</strong>
+					<p>Go to Appearance > Widgets and add some widgets to the "Left Sidebar" </p>  
+				</div>
+ <?php endif; ?>
 </div>
+
--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,10 @@
 <br/><br/>
 <div class="well well-sm">
 <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : ?>
+			<div class="alert alert-warning">
+					<strong>NO WIDGETS FOUND!</strong>
+					<p>Go to Appearance > Widgets and add some widgets to the "Main Sidebar" </p>  
+			</div>
 <?php endif; ?>
 </div>
+
